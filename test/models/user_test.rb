eb28836dd04e3f007a77962a57eb593228130360
raphael <bmiggi@gmail.com>
2019-05-10 01:19:57 +0000

    complete ch 9-10

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -40,11 +40,6 @@ class UserTest < ActiveSupport::TestCase
     end
   end
   
-  test "email addresses should be unique" do
-    duplicate_user = @user.dup
-    @user.save
-    assert_not duplicate_user.valid?
-  end
   
   test "email addresses should be unique" do
     duplicate_user = @user.dup
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   fixtures :all
-
+  include ApplicationHelper
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
